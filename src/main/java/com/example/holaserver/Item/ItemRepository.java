7e94b6a2148cdb7f7ab00d1fc36449ffc39ea089
Song-YunMin <thd930308@naver.com>
2023-04-02 16:06:40 +0900

    [Update] 임시저장 상품 제외 로직 추가

--- src/main/java/com/example/holaserver/Item/ItemRepository.java
+++ src/main/java/com/example/holaserver/Item/ItemRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findItemsByStoreId(Long storeId);
+    List<Item> findItemsByStoreIdAndIsReady(Long storeId, Boolean isReady);
 }
